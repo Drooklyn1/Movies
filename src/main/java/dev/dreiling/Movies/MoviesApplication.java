5285ab401985df416b6bd588ffe75c9104be829b
FX Drooklyn <dr.f380@hotmail.com>
2023-11-01 19:51:54 +0100

    MovieController class

--- src/main/java/dev/dreiling/Movies/MoviesApplication.java
+++ src/main/java/dev/dreiling/Movies/MoviesApplication.java
@@ -2,14 +2,22 @@ package dev.dreiling.Movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
-
+	@GetMapping
+	public ResponseEntity<String> ApiRoot() {
+		return new ResponseEntity<String>("Movies API", HttpStatus.OK);
+	}
 
 }
