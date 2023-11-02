602c68936e251f6043adf2c742a45baeffb6a2ec
FX Drooklyn <dr.f380@hotmail.com>
2023-11-02 23:18:22 +0100

    Review Service, Repository classes GetSingleMovie added

--- src/main/java/dev/dreiling/Movies/MovieService.java
+++ src/main/java/dev/dreiling/Movies/MovieService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,6 +13,10 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 
 }
